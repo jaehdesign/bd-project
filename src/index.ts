537cd220eb1c57397478448c5021d5ccedb3e971
juanan <jaehdesign@gmailcom>
2025-02-20 11:43:34 +0100

    Actualización conexión bd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
-import { loadEnvFile } from 'process';
-loadEnvFile('.env');
+// import { loadEnvFile } from 'process';
+process.loadEnvFile('.env');
 //console.log('Start index.ts');
 
 const dataConnection = {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || '',
@@ -13,7 +13,12 @@ const dataConnection = {
 
 try {
     const connection = await mysql.createConnection(dataConnection);
-    console.log('Connection', connection);
+    console.log(
+        'Connection to server',
+        connection.config.host,
+        connection.config.port,
+    );
+    console.log('Connection to DB', connection.config.database);
 } catch (error) {
     if (error instanceof Error) {
         console.error(error);
